2b03d84dfda1fb7583644a4221813555f4e11dda
Dmitry Pivkin <dmitry.pivkin@simbirsoft.com>
2021-06-11 08:23:19 +0300

    add project

---
+++ HelloSimbirsoft/HelloSimbirsoft/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  HelloSimbirsoft
+//
+//  Created by Dmitry on 11.06.2021.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    let blueColor: UIColor = .link
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
